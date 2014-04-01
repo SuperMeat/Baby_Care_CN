094ebbec144ac37d2ac175560109799bfd1da035
Jamming <arvilinam@gmail.com>
2014-04-01 15:51:17 +0800

    1.0.0 04011551

--- Parenting/Parenting/ActivityViewController.h
+++ Parenting/Parenting/ActivityViewController.h
@@ -2,12 +2,26 @@
 //  ActivityViewController.h
 //  Amoy Baby Care
 //
-//  Created by @Arvi@ on 14-3-18.
+//  Created by @Arvi@ on 14-3-31.
 //  Copyright (c) 2014年 爱摩科技有限公司. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "ACViewController.h"
 
-@interface ActivityViewController : ACViewController
+@interface ActivityViewController : ACViewController<UITableViewDataSource,UITableViewDelegate>
+{
+    NSTimer *timer;
+    UITableView *datatable;
+    NSArray *dataArray;
+}
+
+@property (strong, nonatomic) UIImageView *activityImageView;
+@property (strong, nonatomic) UIButton *btnFeed;
+@property (strong, nonatomic) UIButton *btnBath;
+@property (strong, nonatomic) UIButton *btnPlay;
+@property (strong, nonatomic) UIButton *btnSleep;
+@property (strong, nonatomic) UIButton *btnDiaper;
+@property (strong, nonatomic) UIButton *btnSummary;
+@property (strong, nonatomic) UIButton *btnAdvise;
 
 @end
